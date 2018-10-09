77c384b78b9e864b21997501bf790eb2e8596ab4
rick.parker <rick.parker@r3cev.com>
2018-10-09 16:59:43 +0100

    Reduce references to DefaultSizedCacheFactory to only serialization contexts.

--- testing/test-utils/src/main/kotlin/net/corda/testing/internal/TestingNamedCacheFactory.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/internal/TestingNamedCacheFactory.kt
@@ -1,4 +1,4 @@
-package net.corda.node.utilities
+package net.corda.testing.internal
 
 import com.codahale.metrics.MetricRegistry
 import com.github.benmanes.caffeine.cache.Cache
@@ -8,6 +8,7 @@ import com.github.benmanes.caffeine.cache.LoadingCache
 import net.corda.core.serialization.SingletonSerializeAsToken
 import net.corda.node.services.config.MB
 import net.corda.node.services.config.NodeConfiguration
+import net.corda.node.utilities.BindableNamedCacheFactory
 
 class TestingNamedCacheFactory private constructor(private val sizeOverride: Long, private val metricRegistry: MetricRegistry?, private val nodeConfiguration: NodeConfiguration?) : BindableNamedCacheFactory, SingletonSerializeAsToken() {
     constructor(sizeOverride: Long = 1024) : this(sizeOverride, null, null)
